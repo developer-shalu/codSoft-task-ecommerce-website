9e5ed1934a32e4971ed6f10b5c465994d478e5d5
shalu <meghrajshalu38@gmail.com>
2024-08-29 13:48:06 +0530

    Add files via upload

---
+++ E-commerce website/script.js
@@ -0,0 +1,37 @@
+const imgs=document.querySelectorAll('.header-slider ul img')
+const prev_btn=document.querySelectorAll('.control_prev')
+const next_btn=document.querySelectorAll('.control_next')
+
+let n=0;
+function changeSlide(){
+    for (let i=0; i<imgs.length; i++ ){
+        imgs[i].Style.display='none';
+    }
+    imgs[n].Style.display='block';
+}
+changeSlide();
+
+prev_btn.addEventListener('click',(e)=>{
+    if(n>0){
+        n--;
+    }else{
+        n = imgs.length -1;
+    }
+    changeSlide();
+})
+next_btn.addEventListener('click',(e)=>{
+    if(n <  imgs.length -1){
+        n++;
+    }else{
+        n = 0;
+    }
+    changeSlide();
+})
+
+const scrollContainer = document.querySelectorAll('.products');
+for(const item of scrollContainer){
+item.addEventListener('wheel',(evt)=>{
+    evt.preventDefault();
+    item.scrollLeft += evt.deltaY;
+});
+}
